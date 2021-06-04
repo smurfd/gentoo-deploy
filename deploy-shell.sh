cf2c4101bc32aed4234aa47c763cf8ff857b39ea
Nicklas Boman <smurfd@gmail.com>
2021-06-04 03:36:07 +0200

    fix up-down fuzzy search completion in deploy-shell

--- deploy-shell.sh
+++ deploy-shell.sh
@@ -129,17 +129,14 @@ export PATH=\$HOME/.cargo/bin:\$HOME/.local/bin:\$PATH
 alias ls="ls --color -p"
 
 # start typing + [Up-Arrow] - fuzzy find history forward
-if [[ "\${terminfo[kcuu1]}" != "" ]]; then
-  autoload -U up-line-or-beginning-search
-  zle -N up-line-or-beginning-search
-  bindkey "\${terminfo[kcuu1]}" up-line-or-beginning-search
-fi
+autoload -U up-line-or-beginning-search
+zle -N up-line-or-beginning-search
+bindkey '^[[A' up-line-or-beginning-search # Up
+
 # start typing + [Down-Arrow] - fuzzy find history backward
-if [[ "\${terminfo[kcud1]}" != "" ]]; then
-  autoload -U down-line-or-beginning-search
-  zle -N down-line-or-beginning-search
-  bindkey "\${terminfo[kcud1]}" down-line-or-beginning-search
-fi
+autoload -U down-line-or-beginning-search
+zle -N down-line-or-beginning-search
+bindkey '^[[B' down-line-or-beginning-search # Down
 
 # tab completion
 autoload -U compinit promptinit
